21c09204047abadab8b542f22d01ad8dd78f28f6
wujons <wujohns@163.com>
2018-05-30 10:43:52 +0800

    github相关测试

---
+++ example.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+/**
+ * example to use
+ *
+ * @date 18/5/30
+ */
+
+func main() {
+	fmt.Println("example")
+}
---
+++ streamzip/compress.go
@@ -0,0 +1,16 @@
+package streamzip
+
+import (
+	"fmt"
+)
+
+/**
+ * compress
+ *
+ * @date 18/5/30
+ */
+
+// Compress test
+func Compress() {
+	fmt.Println("just fun")
+}
